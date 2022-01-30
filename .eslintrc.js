c3b1d0bb847ffa271e293281a04ec5a3ab42cce5
Alexander <you@example.com>
2022-01-30 14:23:04 +0200

    eslint added to project

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        'airbnb',
+        'airbnb-typescript',
+        'airbnb/hooks',
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:react-hooks/recommended',
+        'airbnb/rules/react',
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": './tsconfig.json',
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        'import/prefer-default-export': 'off',
+        'react/jsx-props-no-spreading': 'off',
+        'react/function-component-definition': 'off',
+        'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+    },
+    ignorePatterns: ['.eslintrc.js'],
+}
